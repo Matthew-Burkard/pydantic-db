3c1b8677f5f8fedccaf2f829b4798b41b31f080b
Matthew Burkard <matthewjburkard@gmail.com>
2022-05-11 18:58:51 -0400

    test: add one to many unit tests

---
+++ tests/test_many_relations.py
@@ -0,0 +1,64 @@
+"""PyDB tests."""
+from __future__ import annotations
+
+import asyncio
+import unittest
+from uuid import UUID, uuid4
+
+from pydantic import BaseModel, Field
+from sqlalchemy.ext.asyncio import create_async_engine
+
+from pydantic_db.pydb import PyDB
+
+engine = create_async_engine("sqlite+aiosqlite:///db.sqlite3")
+db = PyDB(engine)
+
+
+@db.table(pk="id")
+class One(BaseModel):
+    """One will have many "Many"."""
+
+    id: UUID = Field(default_factory=uuid4)
+    many_a: list[Many] | None = None
+    many_b: list[Many] | None = None
+
+
+@db.table(pk="id")
+class Many(BaseModel):
+    """Has a "One" parent and "Many" siblings."""
+
+    id: UUID = Field(default_factory=uuid4)
+    one_a: One
+    one_b: One
+
+
+class PyDBManyRelationsTests(unittest.IsolatedAsyncioTestCase):
+    def setUp(self) -> None:
+        """Setup clean sqlite database."""
+
+        async def _init() -> None:
+            await db.init()
+            async with engine.begin() as conn:
+                await conn.run_sync(db.metadata.drop_all)
+                await conn.run_sync(db.metadata.create_all)
+
+        asyncio.run(_init())
+
+    async def test_one_to_many(self) -> None:
+        one_a = One()
+        await db[One].insert(one_a)
+        one_b = One()
+        await db[One].insert(one_b)
+        many_a = [Many(one_a=one_a, one_b=one_b), Many(one_a=one_a, one_b=one_b)]
+        many_b = [
+            Many(one_a=one_a, one_b=one_b),
+            Many(one_a=one_a, one_b=one_b),
+            Many(one_a=one_a, one_b=one_b),
+        ]
+        for many in many_a + many_b:
+            await db[Many].insert(many)
+        find_one_a = await db[One].find_one(one_a.id, depth=2)
+        self.assertListEqual(many_a, find_one_a.many_a)
+        self.assertListEqual(many_b, find_one_a.many_b)
+        many_a_idx_zero = await db[Many].find_one(many_a[0].pk, depth=3)
+        self.assertDictEqual(find_one_a.dict(), many_a_idx_zero.one_a.dict())
